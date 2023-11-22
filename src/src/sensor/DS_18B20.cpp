be184ca77994d8b5e86a82779bc969cf09bcb470
krycha88 <krystian.franieczek@gmail.com>
2023-11-22 20:21:07 +0100

    Refaktor odczytywania DS18b20

--- src/src/sensor/DS_18B20.cpp
+++ src/src/sensor/DS_18B20.cpp
@@ -1,86 +1,11 @@
 #include "DS_18B20.h"
 #include "../../SuplaDeviceGUI.h"
 
-
-OneWireBus::OneWireBus(uint8_t pinNumberConfig) : oneWire(pinNumberConfig), pin(pinNumberConfig), nextBus(nullptr), lastReadTime(0) {
-  supla_log(LOG_DEBUG, "Initializing OneWire bus at pin %d", pinNumberConfig);
-  sensors.setOneWire(&oneWire);
-  sensors.begin();
-  if (sensors.isParasitePowerMode()) {
-    supla_log(LOG_DEBUG, "OneWire(pin %d) Parasite power is ON", pinNumberConfig);
-  }
-  else {
-    supla_log(LOG_DEBUG, "OneWire(pin %d) Parasite power is OFF", pinNumberConfig);
-  }
-
-  supla_log(LOG_DEBUG, "OneWire(pin %d) Found %d devices:", pinNumberConfig, sensors.getDeviceCount());
-
-  DeviceAddress address;
-  for (int i = 0; i < sensors.getDeviceCount(); i++) {
-    if (!sensors.getAddress(address, i)) {
-      supla_log(LOG_DEBUG, "Unable to find address for Device %d", i);
-    }
-    else {
-      // Zmiana: Ustawianie rozdzielczości na 12 bitów
-      sensors.setResolution(address, 12);
-    }
-    // Zmiana: Dodanie krótkiego opóźnienia przed pomiarem
-    delay(100);
-  }
-  sensors.setWaitForConversion(true);
-  sensors.requestTemperatures();
-  sensors.setWaitForConversion(false);
-}
-
-int8_t OneWireBus::getIndex(uint8_t *deviceAddress) {
-  DeviceAddress address;
-  for (int i = 0; i < sensors.getDeviceCount(); i++) {
-    if (sensors.getAddress(address, i)) {
-      bool found = true;
-      for (int j = 0; j < 8; j++) {
-        if (deviceAddress[j] != address[j]) {
-          found = false;
-        }
-      }
-      if (found) {
-        return i;
-      }
-    }
-  }
-  return -1;
-}
-
-DS18B20::DS18B20(uint8_t pin, uint8_t *deviceAddress) {
-  OneWireBus *bus = oneWireBus;
-  OneWireBus *prevBus = nullptr;
-  address[0] = 0;
-  lastValidValue = TEMPERATURE_NOT_AVAILABLE;
-  retryCounter = 0;
-
-  if (bus) {
-    while (bus) {
-      if (bus->pin == pin) {
-        myBus = bus;
-        break;
-      }
-      prevBus = bus;
-      bus = bus->nextBus;
-    }
-  }
-
-  // Brak utworzonego magistrali OneWire dla tego pinu
-  if (!bus) {
-    supla_log(LOG_DEBUG, "Creating OneWire bus for pin: %d", pin);
-    myBus = new OneWireBus(pin);
-    if (prevBus) {
-      prevBus->nextBus = myBus;
-    }
-    else {
-      oneWireBus = myBus;
-    }
-  }
+DS18B20::DS18B20(uint8_t pin, uint8_t *deviceAddress)
+    : oneWire(pin), sensors(&oneWire), lastValidValue(TEMPERATURE_NOT_AVAILABLE), retryCounter(0), lastReadTime(0) {
   if (deviceAddress == nullptr) {
-    supla_log(LOG_DEBUG, "Device address not provided. Using device from index 0");
+    Serial.println("Device address not provided. Using device from index 0");
+    address[0] = 0;
   }
   else {
     memcpy(address, deviceAddress, 8);
@@ -92,47 +17,46 @@ void DS18B20::iterateAlways() {
   const unsigned long updateInterval = 5000;
 
   unsigned long currentTime = millis();
-  unsigned long timeSinceLastRead = currentTime - myBus->lastReadTime;
+  unsigned long timeSinceLastRead = currentTime - lastReadTime;
 
   if (timeSinceLastRead >= conversionInterval) {
-    myBus->sensors.setWaitForConversion(true);
-    myBus->sensors.requestTemperatures();
-    myBus->sensors.setWaitForConversion(false);
-    myBus->lastReadTime = currentTime;
+    sensors.setWaitForConversion(true);
+    sensors.requestTemperatures();
+    sensors.setWaitForConversion(false);
+    lastReadTime = currentTime;
   }
 
-  if (timeSinceLastRead >= updateInterval && (lastReadTime != myBus->lastReadTime)) {
+  if (timeSinceLastRead >= updateInterval) {
     channel.setNewValue(getValue());
-    lastReadTime = myBus->lastReadTime;
   }
 }
 
 double DS18B20::getValue() {
   double value = TEMPERATURE_NOT_AVAILABLE;
 
-  // Zmiana: Sprawdzenie dostępności konwersji przed odczytem
-  if (myBus->sensors.isConversionComplete()) {
-    value = myBus->sensors.getTempC(address);
+  if (sensors.isConversionComplete()) {
+    if (address[0] == 0) {
+      value = sensors.getTempCByIndex(0);
+    }
+    else {
+      value = sensors.getTempC(address);
+    }
 
     if (value == DEVICE_DISCONNECTED_C || value == 85.0) {
       value = TEMPERATURE_NOT_AVAILABLE;
     }
 
-    if (value == TEMPERATURE_NOT_AVAILABLE) {
-      retryCounter++;
-      if (retryCounter > 3) {
-        retryCounter = 0;
-        // Zmiana: Ponowne zapytanie o temperatury z opóźnieniem
-        myBus->sensors.setWaitForConversion(true);
-        myBus->sensors.requestTemperatures();
-        myBus->sensors.setWaitForConversion(false);
-      }
-      else {
-        value = lastValidValue;
-      }
+    if (value != TEMPERATURE_NOT_AVAILABLE || retryCounter > 3) {
+      retryCounter = 0;
     }
     else {
-      retryCounter = 0;
+      retryCounter++;
+
+      sensors.setWaitForConversion(true);
+      sensors.requestTemperatures();
+      sensors.setWaitForConversion(false);
+
+      value = lastValidValue;
     }
   }
 
@@ -142,10 +66,6 @@ double DS18B20::getValue() {
 
 void DS18B20::onInit() {
   channel.setNewValue(getValue());
-}
-
-uint8_t DS18B20::getPin() {
-  return myBus->pin;
 }
 
 void DS18B20::setDeviceAddress(uint8_t *deviceAddress) {
@@ -195,6 +115,6 @@ void findAndSaveDS18B20Addresses() {
   Serial.print("Znaleziono łącznie ");
   Serial.print(deviceCount);
   Serial.println(" urządzeń DS18B20.");
-}
 
-OneWireBus *DS18B20::oneWireBus = nullptr;
+  ConfigManager->save();
+}
--- src/src/sensor/DS_18B20.h
+++ src/src/sensor/DS_18B20.h
@@ -8,36 +8,23 @@
 #include <supla-common/log.h>
 #include <supla/sensor/thermometer.h>
 
-class OneWireBus {
- public:
-  OneWireBus(uint8_t pinNumberConfig);
-  int8_t getIndex(uint8_t *deviceAddress);
-
-  OneWire oneWire;
-  uint8_t pin;
-  OneWireBus *nextBus;
-  unsigned long lastReadTime;
-  DallasTemperature sensors;
-};
-
 class DS18B20 : public Supla::Sensor::Thermometer {
+ private:
+  OneWire oneWire;
+  DallasTemperature sensors;
+  double lastValidValue;
+  uint8_t retryCounter;
+  unsigned long lastReadTime;
+  uint8_t address[8];
+
  public:
-  DS18B20(uint8_t pin, uint8_t *deviceAddress = nullptr);
+  DS18B20(uint8_t pin, uint8_t* deviceAddress = nullptr);
   void iterateAlways();
   double getValue();
   void onInit();
-  uint8_t getPin();
-  void setDeviceAddress(uint8_t *deviceAddress = nullptr);
-
- protected:
-  static OneWireBus *oneWireBus;
-  OneWireBus *myBus;
-  DeviceAddress address;
-  int8_t retryCounter;
-  double lastValidValue;
+  void setDeviceAddress(uint8_t* deviceAddress);
 };
 
 void findAndSaveDS18B20Addresses();
-
 
 #endif  // SuplaSensorDS18B20_h

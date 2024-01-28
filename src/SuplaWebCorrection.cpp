36985cd4a04665dac626bb33b2fd219842853c15
krycha88 <krystian.franieczek@gmail.com>
2024-01-28 16:03:12 +0100

    Ukrycie możliwości robienia korekt w GUI

--- src/SuplaWebCorrection.cpp
+++ src/SuplaWebCorrection.cpp
@@ -13,7 +13,7 @@
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
-
+#ifdef SUPLA_GUI_CORRECTION
 #include "SuplaWebCorrection.h"
 
 void createWebCorrection() {
@@ -122,3 +122,4 @@ ThermHygroMeterCorrectionHandler& ThermHygroMeterCorrectionHandler::getInstance(
   static ThermHygroMeterCorrectionHandler handler;
   return handler;
 }
+#endif

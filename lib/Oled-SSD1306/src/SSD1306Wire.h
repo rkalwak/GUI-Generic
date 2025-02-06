739ce760a998118a915d00b8b1052e7da86386d7
krycha88 <krystian.franieczek@gmail.com>
2025-02-06 07:08:22 +0100

    up

--- lib/Oled-SSD1306/src/SSD1306Wire.h
+++ lib/Oled-SSD1306/src/SSD1306Wire.h
@@ -79,11 +79,12 @@ class SSD1306Wire : public OLEDDisplay {
       this->_address = _address;
       this->_sda = _sda;
       this->_scl = _scl;
-#if defined(CONFIG_IDF_TARGET_ESP32C6) || defined(CONFIG_IDF_TARGET_ESP32C3)
-    this->_wire = &Wire;
-#else
-    this->_wire = (_i2cBus == I2C_ONE) ? &Wire : &Wire1;
-#endif
+      this->_wire = &Wire;
+// #if defined(CONFIG_IDF_TARGET_ESP32C6) || defined(CONFIG_IDF_TARGET_ESP32C3)
+//     this->_wire = &Wire;
+// #else
+//     this->_wire = (_i2cBus == I2C_ONE) ? &Wire : &Wire1;
+// #endif
       this->_frequency = _frequency;
     }
 
